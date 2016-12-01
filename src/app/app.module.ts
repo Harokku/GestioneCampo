b45549645025e1243e5f5cbd6500423ea376c3cf
Crenna Simone <crenna.simone@gmail.com>
2016-12-01 23:28:52 +0100

    Scaffolded depot component with initial interface design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DepotComponent } from './depot/depot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepotComponent
   ],
   imports: [
     BrowserModule,
